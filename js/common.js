848b14ed14ff0352993b227321bb8339c1c68210
Виктория Захарова <zaharova181114@gmail.com>
2023-06-21 00:09:19 +0300

    media

--- js/common.js
+++ js/common.js
@@ -6,6 +6,15 @@ $('.reviews-slider').slick({
 	appendArrows: '.reviews-slider__nav',
 	prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#prev"></use></svg></button>',
 	nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>',
+	responsive: [
+		{
+			breakpoint: 576,
+			settings: {
+				slidesToShow: 1,
+				variableWidth: false,
+			}
+		}
+	]
 });
 
 $('.steering-slider').slick({
@@ -14,11 +23,39 @@ $('.steering-slider').slick({
 	appendArrows: '.steering-slider__nav',
 	prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#prev"></use></svg></button>',
 	nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>',
+	responsive: [
+		{
+			breakpoint: 576,
+			settings: {
+				slidesToShow: 1,
+				variableWidth: false,
+			}
+		}
+	]
 });
 
 $(".countdown-time").countdowntimer({
-	dateAndTime: "2023/06/21 00:00:00",
+	dateAndTime: "2023/06/22 00:00:00",
 	labelsFormat: true,
 	displayFormat: "HMS",
-	// currentTime: true,
+});
+
+$('.btn-burger').on('click', function () {
+	$('.mobile-menu').fadeToggle();
+});
+
+$('.mobile-menu__close').on('click', function () {
+	$('.mobile-menu').fadeOut();
+});
+
+$('.go_to').click(function (e) {
+	e.preventDefault();
+	var scroll_el = $(this).attr('href');
+	if ($(scroll_el).length !== 0) {
+		$('html, body').animate({
+			scrollTop: $(scroll_el).offset().top
+		}, 500);
+	}
+	$('.mobile-menu').fadeOut();
+	return false;
 });
